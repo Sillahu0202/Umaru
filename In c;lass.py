dce64e0674dfeba9dafd18d3950252861e3a7c07
Umaru Sillah <SILLAHU0202@students.bowiestate.edu>
2023-10-02 18:11:12 -0400

    commit for in-class and Lab2 partB files

---
+++ In c;lass.py
@@ -0,0 +1,13 @@
+input1 = 5
+input2 = 6
+output = input1 + input2
+print (input1,input2,output)
+
+firstinput = 5
+secondinput = 6
+thirdinput = 7
+fourthinput = 8
+output1 = firstinput + secondinput + thirdinput + fourthinput
+output2 = firstinput * secondinput * thirdinput * fourthinput
+print(output1 ,output2)
+
---
+++ Lab2 partB.py
@@ -0,0 +1,36 @@
+input1 = 5
+input2 = 10
+add = input1+input2
+multiplication = input2*input1
+subtraction = input1-input2
+division = input2/input1
+print(add, multiplication, subtraction, division)
+
+number1 = 10
+number2 = 11
+number3 = 12
+number4 = 13
+add = number1 + number2 + number3 + number4
+multiplication = number1 * number2 * number3 * number4
+subtraction = number4 - number3 - number2 - number1
+division = (number4/number3)
+print(add, multiplication, subtraction, division)
+
+number1 = 13
+number2 = 12
+number3 = 11
+number4 = 10
+add = number1 + number2 + number3 + number4
+multiplication = number1 * number2 * number3 * number4
+subtraction = number4 - number3 - number2 - number1
+division = (number4/number3)
+print(add, multiplication, subtraction, division)
+
+#Revising the above steps mentioned.
+add = number1 + number2
+multiplication = number3 * number4
+subtraction = number1 - number2
+division = (number1/number2)
+print(add, multiplication, subtraction, division)
+
+
