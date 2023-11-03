0e25d909dbe051fda7baab0f2c0710c6a8ed05d2
Umaru Sillah <SILLAHU0202@students.bowiestate.edu>
2023-11-03 02:41:08 -0400

    commit for Exam part 2 Q3

---
+++ Exam Part 2 Q3.py
@@ -0,0 +1,15 @@
+""""Collect five input from users
+calculate sum and Average """
+
+number1= int(input("Enter number1: "))
+number2= int(input("Enter number2: "))
+number3= int(input("Enter number3: "))
+number4= int(input("Enter number4: "))
+number5= int(input("Enter number5: "))
+
+add= (number1+number2+number3+number4+number5)
+print(add)
+
+# Calculate the average value
+average=add/5
+print(int(average))
