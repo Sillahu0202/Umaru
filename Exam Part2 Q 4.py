57bd4a8a5da2e2e20126cda938cd1c7038d03b01
Umaru Sillah <SILLAHU0202@students.bowiestate.edu>
2023-11-03 03:04:29 -0400

    commit for Exam part 2 Q4

---
+++ Exam Part2 Q 4.py
@@ -0,0 +1,15 @@
+""" Demonstration of a for loop"""
+
+# Assign a value to the variable B
+B= 12
+# iteration statement
+for count in range(1,10):
+    B=B*count
+    print(B)
+
+"""B=12*count(1,10)
+B=12*1=12
+B=12
+B=12*2=24
+B=24
+"""""
