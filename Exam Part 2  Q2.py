50edd6f9a1c8ef3cb23afdbe5b3812ba75c84131
Umaru Sillah <SILLAHU0202@students.bowiestate.edu>
2023-11-03 02:11:04 -0400

    commit for Exam part 2 Q1

---
+++ Exam Part 2  Q2.py
@@ -0,0 +1,13 @@
+""""Calculate the area of a circle
+collect input from user (radius)
+Use math function"""
+
+# Area of a circle is pi.r**2
+import math
+# print math.pi to confirm successful math lib. import
+print(math.pi)
+radius = int(input("Enter the radius:\n"))
+areaofCircle =("pi * radius**2")
+pi = input(math.pi)
+areaofCircle = (math.pi * radius**2)
+print(areaofCircle)
