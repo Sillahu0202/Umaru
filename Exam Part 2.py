94a7d64e3d6f8e52a78790edd7faa1c86affdb75
Umaru Sillah <SILLAHU0202@students.bowiestate.edu>
2023-11-03 01:28:43 -0400

    commit for Exam part 2 Q1

---
+++ Exam Part 2.py
@@ -0,0 +1,9 @@
+#Assigning values to two variables
+length=int(input("Enter the length of a variable:"))
+breadth=int(input("Enter the breadth of a variable:"))
+""" Note: Formula to calculate the area of a rectangle is:
+area=length*breadth
+"""
+Area=length*breadth
+print(Area)
+
